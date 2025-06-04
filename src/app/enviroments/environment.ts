959e059c9cf6b06c135ba86854a578c03ea0e4fa
Sashaaasssss <bazhenovsasha17052006@gmail.com>
2025-06-04 19:53:33 +0300

    url

--- src/app/enviroments/environment.ts
+++ src/app/enviroments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://127.0.0.1:8000' // Замените на ваш базовый URL API
+    apiUrl: 'https://fd5c83b9957922313c6eef0c54227816.serveo.net' // Замените на ваш базовый URL API
   };
 
   // https://api.excellentjewellery.ru/api
